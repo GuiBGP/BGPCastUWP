09da15cf65a47eaa1eaaaab28d8bceeef81f5ab3
GuiBGP <guilhermebgp@gmail.com>
2016-04-17 12:36:39 -0300

    Criação da AddFeed.

--- Interface/UWP/App.xaml.cs
+++ Interface/UWP/App.xaml.cs
@@ -79,7 +79,7 @@ namespace BGPCastUWP.Interface.UWP
             {
                 // When the navigation stack isn't restored, navigate to the first page
                 // suppressing the initial entrance animation.
-                shell.AppFrame.Navigate(typeof(WhatPlayPage), e.Arguments, new Windows.UI.Xaml.Media.Animation.SuppressNavigationTransitionInfo());
+                shell.AppFrame.Navigate(typeof(AddFeedPage), e.Arguments, new Windows.UI.Xaml.Media.Animation.SuppressNavigationTransitionInfo());
             }
 
             // Ensure the current window is active
--- Interface/UWP/Views/AddFeedPage.xaml.cs
+++ Interface/UWP/Views/AddFeedPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,41 @@ namespace BGPCastUWP.Interface.UWP.Views
         public AddFeedPage()
         {
             this.InitializeComponent();
+        }
+
+        private void FindAppBarButtom_Click(object sender, RoutedEventArgs e)
+        {
+            FindAppBarButtom.Visibility = Visibility.Collapsed;
+            AddTextBlock.Visibility = Visibility.Collapsed;
+            CancelAppBarButtom.Visibility = Visibility.Visible;
+            SearchTextBox.Visibility = Visibility.Visible;
+            SearchTextBox.Focus(FocusState.Programmatic);
+
+            //TopCommandBar.SecondaryCommands.ToList().ForEach(c => ((AppBarButton)c).Visibility = Visibility.Collapsed);
+        }
+
+        private void CancelAppBarButtom_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(SearchTextBox.Text))
+            {
+                FindAppBarButtom.Visibility = Visibility.Visible;
+                AddTextBlock.Visibility = Visibility.Visible;
+                CancelAppBarButtom.Visibility = Visibility.Collapsed;
+                SearchTextBox.Visibility = Visibility.Collapsed;
+            }
+            else {
+                SearchTextBox.Text = String.Empty;
+                SearchTextBox.Focus(FocusState.Programmatic);
+            }
+        }
+
+        private async void SearchTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter && !String.IsNullOrWhiteSpace(SearchTextBox.Text))
+            {
+                e.Handled = true;
+                await new MessageDialog("Downloading!!!").ShowAsync();
+            }
         }
     }
 }
