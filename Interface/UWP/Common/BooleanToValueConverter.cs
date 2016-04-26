ab33ff4ccd6ba9f65f6cf7aefd4ba363e8eeae7d
GuiBGP <guilhermebgp@gmail.com>
2016-04-25 23:09:46 -0300

    Criação de alguns Converters.

---
+++ Interface/UWP/Common/BooleanToValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BGPCastUWP.Interface.UWP.Common
+{
+    class BooleanToValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            String[] parameters = (parameter as string ?? string.Empty).Split('|');
+            return (bool)value ? parameters.First() : parameters.Last();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) { 
+            throw new NotImplementedException();
+        }
+    }
+}
